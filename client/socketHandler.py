b87487d0fe1ea1552251919af8c020cbfbca2ae8
kisielski <krzysiuszymankiewicz@gmail.com>
2022-06-12 13:27:54 +0200

    cbc
    cbc and picking other client

--- client/socketHandler.py
+++ client/socketHandler.py
@@ -6,12 +6,20 @@ import logging
 import pickle
 import time
 import math
+import base64
 from tkinter import END
+from config import *
 
 from message.message import Message, MessageType
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes, padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -27,23 +35,38 @@ class SocketHandler:
         self.isConnectionActive = False
         self.output = output
         self.publicKeys = {}
+        self.otherClients = []
 
-        self.key = rsa.generate_private_key(
-            backend=crypto_default_backend(),
-            public_exponent=65537,
-            key_size=2048
-        )
+        privateKeyFile = open("privateKey.key", "wb")
+        publicKeyFile = open("publicKey.key", "wb")
 
-        self.privateKey = self.key.private_bytes(
-            crypto_serialization.Encoding.PEM,
-            crypto_serialization.PrivateFormat.PKCS8,
-            crypto_serialization.NoEncryption()
-        )
+        if os.path.getsize("privateKey.key") == 0 or os.path.getsize("publicKey.key") == 0:
+            self.key = rsa.generate_private_key(
+                backend=crypto_default_backend(),
+                public_exponent=65537,
+                key_size=2048
+            )
 
-        self.publicKey = self.key.public_key().public_bytes(
-            crypto_serialization.Encoding.OpenSSH,
-            crypto_serialization.PublicFormat.OpenSSH
-        )
+            self.privateKey = self.key.private_bytes(
+                crypto_serialization.Encoding.PEM,
+                crypto_serialization.PrivateFormat.PKCS8,
+                crypto_serialization.NoEncryption()
+            )
+
+            self.publicKey = self.key.public_key().public_bytes(
+                crypto_serialization.Encoding.OpenSSH,
+                crypto_serialization.PublicFormat.OpenSSH
+            )
+
+            privateKeyFile.write(self.encryptText(localPassword, self.privateKey))
+            publicKeyFile.write(self.encryptText(localPassword, self.publicKey))
+        else:
+            self.privateKey = self.decryptText(localPassword, privateKeyFile)
+            self.publicKey = self.decryptText(localPassword, publicKeyFile)
+
+        privateKeyFile.close()
+        publicKeyFile.close()
+
         self.sessionKeys = {}
 
         self.connectionThread = threading.Thread(target=self.connect)
@@ -78,7 +101,7 @@ class SocketHandler:
                     log.info(f'Error receiving message {e}')
 
     def getNextMessage(self):
-        log.info('Received message')
+        #log.info('Received message')
         msg = self.socket.recv(1024)
         msgLength = int(msg.decode('utf-8'))
         msg = self.socket.recv(msgLength)
@@ -86,8 +109,9 @@ class SocketHandler:
 
     def handleMessage(self, message):
         log.info(f"Received message {message.type}")
+        log.info(f"Received message {message.content}")
         if message.type == MessageType.TEXT:
-            self.output.insert(END, f"\n [{message.sender[0]}:{message.sender[1]}] {message.content}")
+            self.receiveTextMessage(message)
         if message.type == MessageType.FILE:
             self.saveToFile(message)
         if message.type == MessageType.SESSION:
@@ -95,14 +119,19 @@ class SocketHandler:
         if message.type == MessageType.KEY:
             self.saveSessionKey(message)
 
+    def receiveTextMessage(self, message):
+        decryptedContent = self.decryptTextCBC(message.content, self.sessionKeys[message.sender])
+        self.output.insert(END, f"\n [{message.sender[0]}:{message.sender[1]}] {decryptedContent}")
+
     def saveKeyAndSend(self, message):
         if message.sender not in self.publicKeys:
+            self.otherClients.append(message.sender)
             self.publicKeys[message.sender] = message.content.encode("utf-8")
-            msg = Message(self.socket.getsockname(), MessageType.SESSION, self.publicKey.decode("utf-8"))
+            msg = Message(self.socket.getsockname(), MessageType.SESSION, self.publicKey.decode("utf-8"), message.sender)
             self.sendMessage(msg)
         else:
             self.sessionKeys[message.sender] = os.urandom(16)
-            msg = Message(self.socket.getsockname(), MessageType.KEY, self.sessionKeys[message.sender].decode("latin-1"))
+            msg = Message(self.socket.getsockname(), MessageType.KEY, self.sessionKeys[message.sender].decode("latin-1"), message.sender)
             self.sendMessage(msg)
 
     def saveSessionKey(self, message):
@@ -118,12 +147,13 @@ class SocketHandler:
             file.write(b''.join(messages))
         log.info(f"Wrote {len(messages)} parts")
 
-    def sendTextMessage(self, content):
+    def sendTextMessage(self, content, receiver, encryption):
         log.info('TypedEnter')
         if self.isConnectionActive:
             log.info('Sending text message')
+            encryptedContent = self.encryptTextCBC(content, self.sessionKeys[self.findSessionKey(receiver)])
             try:
-                msg = Message(self.socket.getsockname(), MessageType.TEXT, content)
+                msg = Message(self.socket.getsockname(), MessageType.TEXT, encryptedContent, receiver)
                 self.sendMessage(msg)
             except ConnectionResetError as e:
                 self.isConnectionActive = False
@@ -153,7 +183,71 @@ class SocketHandler:
         return bytes
 
     def sendMessage(self, message):
+        # TODO cipher message
         dumped = pickle.dumps(message)
         log.info(f'sending message: {len(dumped)} bytes')
+        log.info(f'sending message: {message.content}')
         self.socket.sendall(self.nextMessageSize(dumped))
         self.socket.sendall(dumped)
+
+    def encryptText(self, password, plaintext):
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=os.urandom(16),
+            iterations=390000,
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
+        fernet = Fernet(key)
+        encrypted = fernet.encrypt(plaintext)
+        return encrypted
+
+    def decryptText(self, password, ciphertext):
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=os.urandom(16),
+            iterations=390000,
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
+        fernet = Fernet(key)
+        plaintext = fernet.decrypt(ciphertext)
+        return plaintext
+
+    def encryptTextCBC(self, plaintext, sessionkey):
+        key = sessionkey
+        iv = os.urandom(16)
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+        encryptor = cipher.encryptor()
+        padded_data = self.pad(iv + plaintext.encode("utf-8"), 16)
+        log.info(iv + plaintext.encode("utf-8"))
+        ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+        return ciphertext
+
+    def decryptTextCBC(selfself, ciphertext, sessionkey):
+        key = sessionkey
+        unpadder = padding.PKCS7(16).unpadder()
+        data = unpadder.update(ciphertext)
+        ciphertext = data + unpadder.finalize()
+        cipher = Cipher(algorithms.AES(key), modes.CBC(ciphertext[:16]))
+        decryptor = cipher.decryptor()
+        plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+        plaintext = (plaintext[17:]).decode("utf-8")
+        return plaintext
+
+    def findSessionKey(self, receiverStr):
+        for e in self.otherClients:
+            if str(e) == receiverStr:
+                return e
+        return None
+
+    def pad(self, str, mul):
+        currLen = len(str)
+        neededBytes = (int(currLen / mul) + 1) * mul - currLen
+        for i in range(neededBytes):
+            str += b"1"
+        log.info(neededBytes)
+        log.info(str)
+        return str
+
+    #def unpad(selfself, str, mul):
